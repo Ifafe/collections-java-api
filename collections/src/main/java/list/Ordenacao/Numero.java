7aa27944edc5a393fde30d2497b4cfb48d3c286b
Ifafe07 <87419215+Ifafe@users.noreply.github.com>
2025-08-07 11:38:41 +0100

    Refactor Numero class to encapsulate numeroList and add getter/setter methods

--- collections/src/main/java/list/Ordenacao/Numero.java
+++ collections/src/main/java/list/Ordenacao/Numero.java
@@ -1,5 +1,47 @@
 package main.java.list.Ordenacao;
 
-public class Numero {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
+public class Numero {
+private List<Integer> numeroList;
+
+    public Numero(List<Integer> numeroList) {
+        this.numeroList = numeroList;
+    }
+
+    public List<Integer> getNumeroList() {
+        return numeroList;
+    }
+
+    public void setNumeroList(List<Integer> numeroList) {
+        this.numeroList = numeroList;
+    }
+
+    @Override
+    public String toString() {
+          return "Numero{" +
+                      "numeroList=" + numeroList +
+                      '}';
+    }
+    
+public static void main(String[] args) {
+        // This is where you can test the Numero class
+        List<Integer> numeros = Arrays.asList(1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+
+        // Example of lambda expression to filter pares numbers 
+        Consumer<Integer> pares = numero -> {
+            if (numero % 2 == 0) System.out.println(numero);
+        };
+            
+
+        // Using the Consumer with the forEach method
+        numeros.forEach(p -> {
+            if (p % 2 == 0) {
+                System.out.println(p);
+            }
+        });
+    }
 }
